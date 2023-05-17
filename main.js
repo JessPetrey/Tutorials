3d2cef6b842d154000f5e2214fbe828c85320fde
Jessica Petrey <jesipetrey@gmail.com>
2023-05-17 15:36:07 -0700

    added documentation comment

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/* This code is creating an Electron application window with a width of 800 pixels and a height of 600
+pixels. It is also loading an HTML file called "index.html" into the window. The `app.whenReady()`
+method is used to ensure that the window is created when the Electron application is ready to start. */
 const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
